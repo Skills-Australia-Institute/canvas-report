3bab8ecac051956313abd1526542c1d9485ada1a
Sanam Limbu <sudosanam@gmail.com>
2024-09-02 15:11:17 +0800

    added sorting by name, course or section in ungraded assignments report

--- web/src/components/reports/ungradedAssignments.tsx
+++ web/src/components/reports/ungradedAssignments.tsx
@@ -1,7 +1,19 @@
-import { Button, Progress, ScrollArea, Table } from '@radix-ui/themes';
+import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons';
+import {
+  Button,
+  Flex,
+  Progress,
+  ScrollArea,
+  Table,
+  Tooltip,
+} from '@radix-ui/themes';
 import { useQueries } from '@tanstack/react-query';
+import { useState } from 'react';
 import { CSVLink } from 'react-csv';
-import { getUngradedAssignmentsByCourseID } from '../../canvas/assignments';
+import {
+  getUngradedAssignmentsByCourseID,
+  UngradedAssignmentWithAccountCourseInfo,
+} from '../../canvas/assignments';
 import { Course } from '../../canvas/courses';
 import { APP } from '../../constants';
 import { useSupabase } from '../../hooks/supabase';
@@ -48,6 +60,7 @@ export default function UngradedAssignments({
   });
 
   const data = result.flat();
+
   const accountName = getFormattedName(account.name);
 
   const errorsOnly = errors.filter((err) => err !== null);
@@ -76,52 +89,196 @@ export default function UngradedAssignments({
     );
   }
 
-  if (data.length < 0) {
-    return <></>;
+  return (
+    <UngradedAssignmentsTable
+      isAllSuccess={isAllSuccess}
+      data={data}
+      accountName={accountName}
+      successCount={successCount}
+      coursesCount={courses.length}
+    />
+  );
+}
+
+interface UngradedAssignmentsTableProps {
+  isAllSuccess: boolean;
+  data: UngradedAssignmentWithAccountCourseInfo[];
+  accountName: string;
+  successCount: number;
+  coursesCount: number;
+}
+type SortBy =
+  | 'course-asc'
+  | 'course-desc'
+  | 'name-asc'
+  | 'name-desc'
+  | 'section-asc'
+  | 'section-desc';
+
+const UngradedAssignmentsTable = ({
+  isAllSuccess,
+  data,
+  accountName,
+  successCount,
+  coursesCount,
+}: UngradedAssignmentsTableProps) => {
+  const [sortBy, setSortBy] = useState<SortBy>('section-asc');
+
+  let sorted: UngradedAssignmentWithAccountCourseInfo[] = [];
+
+  if (isAllSuccess) {
+    sorted = sort(data, sortBy);
   }
 
   return (
     <div>
       <ScrollArea scrollbars="both" className="pr-4" style={{ height: 600 }}>
-        <Table.Root size="1">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Account</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Course</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Section</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Needs Grading</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Teachers</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Gradebook URL</Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {data.map((a) => (
-              <Table.Row key={a.account + a.course_name + a.section + a.name}>
-                <Table.Cell className="max-w-sm">{a.account}</Table.Cell>
-                <Table.Cell className="max-w-sm">{a.course_name}</Table.Cell>
-                <Table.Cell className="max-w-sm">{a.name}</Table.Cell>
-                <Table.Cell className="max-w-sm">{a.section}</Table.Cell>
-                <Table.Cell>{a.needs_grading_section}</Table.Cell>
-                <Table.Cell className="max-w-sm">{a.teachers}</Table.Cell>
-                <Table.Cell>
-                  <a
-                    href={a.gradebook_url}
-                    target="_blank"
-                    className="underline"
+        {data.length > 0 && (
+          <Table.Root size="1">
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Account</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>
+                  <Tooltip
+                    content={`Click to sort by course ${
+                      sortBy === 'course-asc'
+                        ? 'descending'
+                        : sortBy === 'course-desc'
+                        ? 'ascending'
+                        : 'ascending'
+                    }`}
                   >
-                    Link
-                  </a>
-                </Table.Cell>
+                    <Flex
+                      align="center"
+                      gap="1"
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (sortBy === 'course-asc') {
+                          setSortBy('course-desc');
+                        } else if (sortBy === 'course-desc') {
+                          setSortBy('course-asc');
+                        } else {
+                          setSortBy('course-asc');
+                        }
+                      }}
+                    >
+                      <span>Course</span>
+
+                      {sortBy === 'course-asc' && (
+                        <ArrowUpIcon className="cursor-pointer text-blue-500" />
+                      )}
+
+                      {sortBy === 'course-desc' && (
+                        <ArrowDownIcon className="cursor-pointer text-blue-500" />
+                      )}
+                    </Flex>
+                  </Tooltip>
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>
+                  <Tooltip
+                    content={`Click to sort by name ${
+                      sortBy === 'name-asc'
+                        ? 'descending'
+                        : sortBy === 'name-desc'
+                        ? 'ascending'
+                        : 'ascending'
+                    }`}
+                  >
+                    <Flex
+                      align="center"
+                      gap="1"
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (sortBy === 'name-asc') {
+                          setSortBy('name-desc');
+                        } else if (sortBy === 'name-desc') {
+                          setSortBy('name-asc');
+                        } else {
+                          setSortBy('name-asc');
+                        }
+                      }}
+                    >
+                      <span>Name</span>
+
+                      {sortBy === 'name-asc' && (
+                        <ArrowUpIcon className="cursor-pointer text-blue-500" />
+                      )}
+
+                      {sortBy === 'name-desc' && (
+                        <ArrowDownIcon className="cursor-pointer text-blue-500" />
+                      )}
+                    </Flex>
+                  </Tooltip>
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>
+                  <Tooltip
+                    content={`Click to sort by section ${
+                      sortBy === 'section-asc'
+                        ? 'descending'
+                        : sortBy === 'section-desc'
+                        ? 'ascending'
+                        : 'ascending'
+                    }`}
+                  >
+                    <Flex
+                      align="center"
+                      gap="1"
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (sortBy === 'section-asc') {
+                          setSortBy('section-desc');
+                        } else if (sortBy === 'section-desc') {
+                          setSortBy('section-asc');
+                        } else {
+                          setSortBy('section-asc');
+                        }
+                      }}
+                    >
+                      <span>Section</span>
+
+                      {sortBy === 'section-asc' && (
+                        <ArrowUpIcon className="cursor-pointer text-blue-500" />
+                      )}
+
+                      {sortBy === 'section-desc' && (
+                        <ArrowDownIcon className="cursor-pointer text-blue-500" />
+                      )}
+                    </Flex>
+                  </Tooltip>
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Needs Grading</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Teachers</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Gradebook URL</Table.ColumnHeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+            </Table.Header>
+            <Table.Body>
+              {sorted.map((a) => (
+                <Table.Row key={a.account + a.course_name + a.section + a.name}>
+                  <Table.Cell className="max-w-sm">{a.account}</Table.Cell>
+                  <Table.Cell className="max-w-sm">{a.course_name}</Table.Cell>
+                  <Table.Cell className="max-w-sm">{a.name}</Table.Cell>
+                  <Table.Cell className="max-w-sm">{a.section}</Table.Cell>
+                  <Table.Cell>{a.needs_grading_section}</Table.Cell>
+                  <Table.Cell className="max-w-sm">{a.teachers}</Table.Cell>
+                  <Table.Cell>
+                    <a
+                      href={a.gradebook_url}
+                      target="_blank"
+                      className="underline"
+                    >
+                      Link
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        )}
       </ScrollArea>
       <div className="mt-4 border-t pt-4">
         {!isAllSuccess && (
           <Progress
-            value={(successCount / courses.length) * 100}
+            value={(successCount / coursesCount) * 100}
             size="3"
             className="max-w-3xl mt-4"
             color="green"
@@ -130,7 +287,7 @@ export default function UngradedAssignments({
         {isAllSuccess && APP === 'sai' && (
           <>
             <CSVLink
-              data={data.filter(
+              data={sorted.filter(
                 (d) =>
                   !(
                     d.section.includes('ADL') ||
@@ -147,7 +304,7 @@ export default function UngradedAssignments({
               </Button>
             </CSVLink>
             <CSVLink
-              data={data.filter(
+              data={sorted.filter(
                 (d) =>
                   d.section.includes('ADL') ||
                   d.section.includes('Adl') ||
@@ -165,7 +322,7 @@ export default function UngradedAssignments({
         )}
         {isAllSuccess && APP === 'stanley' && (
           <CSVLink
-            data={data}
+            data={sorted}
             headers={headers}
             filename={`${accountName}_ungraded_assignments-${getDateTimeString()}`}
           >
@@ -177,6 +334,27 @@ export default function UngradedAssignments({
       </div>
     </div>
   );
+};
+
+function sort(data: UngradedAssignmentWithAccountCourseInfo[], by: SortBy) {
+  const sorted = [...data];
+
+  switch (by) {
+    case 'course-asc':
+      return sorted.sort((a, b) => a.course_name.localeCompare(b.course_name));
+    case 'course-desc':
+      return sorted.sort((a, b) => b.course_name.localeCompare(a.course_name));
+    case 'name-asc':
+      return sorted.sort((a, b) => a.name.localeCompare(b.name));
+    case 'name-desc':
+      return sorted.sort((a, b) => b.name.localeCompare(a.name));
+    case 'section-asc':
+      return sorted.sort((a, b) => a.section.localeCompare(b.section));
+    case 'section-desc':
+      return sorted.sort((a, b) => b.section.localeCompare(a.section));
+    default:
+      return sorted;
+  }
 }
 
 const headers = [
